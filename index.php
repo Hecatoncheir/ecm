ba9c5068dd62a3fbec01fb6adc50b3d11c2701eb
Vitaliy Vostrikov <rasart.pro@gmail.com>
2014-09-01 17:19:47 +0400

    added php forms

---
+++ index.php
@@ -0,0 +1,278 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Поможем в выборе системы электронного документооборота</title>
+
+    <!-- Add to homescreen for Chrome on Android -->
+    <meta name="mobile-web-app-capable" content="yes">
+    <link rel="icon" sizes="192x192" href="images/touch/chrome-touch-icon-192x192.png">
+
+    <!-- Add to homescreen for Safari on iOS -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-title" content="Web Starter Kit">
+    <link rel="apple-touch-icon-precomposed" href="images/touch/apple-touch-icon-precomposed.png">
+
+    <!-- Tile icon for Win8 (144x144 + tile color) -->
+    <meta name="msapplication-TileImage" content="images/touch/ms-touch-icon-144x144-precomposed.png">
+    <meta name="msapplication-TileColor" content="#3372DF">
+
+    <!-- SEO: If your mobile URL is different from the desktop URL, add a canonical link to the desktop page https://developers.google.com/webmasters/smartphone-sites/feature-phones -->
+    <!--
+    <link rel="canonical" href="http://www.example.com/">
+    -->
+
+      <!-- Add jQuery library -->
+      <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+      <link rel="stylesheet" href="css/jquery.fancybox.css">
+      <script src="scripts/jquery.fancybox.js"></script>
+
+    <link rel="stylesheet" href="css/main.css" type="text/css" />
+
+  </head>
+  <body>
+
+<!--HEADER-->
+    <header>
+        <div id="header-section">
+            <section>
+                <div class="top_info">
+                    <nav>
+                        <a href="tel:+74993909477">
+                            <i></i>
+                            <span>+7 (499) 390-94-77</span>
+                        </a>
+
+                        <a href="mailto:rasart.pro@gmail.com?subject=Письмо%20с%20LP%20ECM">
+                            <i></i>
+                            <span>Написать письмо</span>
+                        </a>
+                    </nav>
+
+                    <a href="#" class="logo"></a>
+                </div>
+
+                <h1>
+                    Объективно помогаем в выборе систем <br>
+                    электронного документооборота.
+                </h1>
+
+                <form id="form_zvonok2" name="form_zvonok2" action="send_zakaz_zvonka.php" method="post" enctype="multipart/form-data">
+                    <input type="text"  name="fio1" size="40" maxlength="40" placeholder="ИМЯ">
+                    <input type="text" id="phone1"  class="input_kz1"  name="phone1" placeholder="ТЕЛЕФОН" required>
+                    <input type="submit" class="ActionButton" name="zvonok_submit" value="" onClick="return Formdata(this.form)">
+                </form>
+
+                <div class="main-info">
+                    <div>
+                        <ul>
+                            <li>
+                                <span>Подбираем СЭД из <span>12 лидирующих</span> на рынке, нужную именно Вам.</span>
+                            </li>
+
+                            <li class="marked-have">
+                                <span class="marked">
+                                    Работаем с СЭД более 7 лет.
+                                </span>
+                            </li>
+
+                            <li>
+                                <span>В области СЭД работает более 50 сотрудников.</span>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                    <div>
+                        <ul>
+                            <li>
+                                <span>Не продвигаем какую-то конкретную СЭД. Для нас все равны.</span>
+                            </li>
+                            <li class="marked-have">
+                                <span class="marked">Консультируем бесплатно.</span>
+                            </li>
+                            <li>
+                                <span>Не навязываем себя на работы по внедрению СЭД.</span>
+                            </li>
+                            <li>
+                                <span>Оказали помощь в выборе СЭД сотням компаний.</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </section>
+        </div>
+    </header>
+
+<!--SLIDE 1-->
+  <section id="slide-1">
+      <section>
+          <h2>Подбираем СЭД, нужную именно Вам</h2>
+          <div class="video">
+              <a class="various fancybox.iframe" href="http://www.youtube.com/embed/x4ff476Cpho?autoplay=1"></a>
+          </div>
+          <div class="text">
+              <p>Вы стоите перед задачей выбора СЭД? На рынке более 70 систем.  Как выбрать подходящую именно Вам?</p>
+
+                 <p> Каждый хвалит свою систему, объективную информацию получить непросто. Все системы концептуально похожи, разница в нюансах. Малозначительные, на первый взгляд, нюансы могут обернуться в дальнейшем очень значительными проблемами в эксплуатации. </p>
+          </div>
+          <div class="text" style="margin-left: 40px;">
+              <p>Поможем Вам сэкономить время на выбор СЭД и деньги на покупку системы.</p>
+
+                 <p> За 7 лет мы сделали более сотни проектов на самых популярных СЭД, с остальными хорошо познакомились на уровне понимания функционала на этапе выбора СЭД для заказчиков.
+              </p>
+          </div>
+          <div class="footer-text">
+              <p>Понимая функционал фактически всех имеющихся на рынке СЭД и большой опыт внедрения различных продуктов, мы можем предложить Вам услугу по подбору СЭД (или группы СЭД), подходящей именно под Ваши потребности.</p>
+          </div>
+          <div class="offer">
+                  <h3>Наше предложение:</h3>
+              <p>
+                  <span>1.</span> Бесплатный подбор подходящей для Вас СЭД.<br>
+                  <span>2.</span> Объективный подбор. Мы независимы и не заинтересованы в продвижении какой-то одной определенной СЭД.
+              </p>
+          </div>
+      </section>
+  </section>
+
+<!--SLIDE 2-->
+  <section id="slide-2">
+      <section>
+        <h2><span>C какими СЭД работаем</span></h2>
+         <p><span>DIRECTUM, DocsVision, 1C Документооборот, ELMA, EOS, WSS Docs, ЕФРАТ, МОТИВ, ДЕЛО, Босс референт, Optima, LanDocs.</span></p>
+
+             <p>Мы знакомы, более чем с 70 систем электронного документооборота, но будем честны – мы имеем большой практический опыт работы только с 12 вышеперечисленными, поэтому и рекомендовать будем наиболее подходящую Вам систему только из этой дюжины.
+         </p>
+
+      </section>
+  </section>
+
+<!--SLIDE 3-->
+  <section id="slide-3">
+    <section>
+      <div class="why">
+          <div class="why-text">
+              <h4>Почему все СЭД для нас равны.</h4>
+              <p>Нашу объективность обеспечивает практически равные финансовые отношения с ведущими разработчиками СЭД в России, поэтому с коммерческой точки зрения нам не принципиально, какую систему Вы выберите. Главный критерий – система должна подходить именно Вам.</p>
+          </div>
+
+          <div class="why-text">
+              <h4><span>Почему для Вас наши услуги бесплатны.</span></h4>
+              <p>Наши услуги бесплатны только для Вас. Нашу работу по подбору СЭД оплачивает разработчик той СЭД, которую Вы выберите. И т.к. размер нашего вознаграждения примерно одинаковый у каждого разработчика СЭД, то все СЭД находятся в равных условиях при осуществлении выбора.</p>
+          </div>
+      </div>
+    </section>
+  </section>
+
+<!--SLIDE 4-->
+<section id="slide-4">
+    <section>
+        <h2><span>Чем мы отличаемся от других компаний?</span></h2>
+        <div class="dif-icons">
+            <span>Мы не будем рекламировать Вам возможности <br> какой-то, отдельной системы</span>
+            <span>Мы не будем предлагать Вам решения с избыточным, ненужным, функционалом</span>
+            <span>Подберём наиболее оптимальную для Вас систему </span>
+        </div>
+    </section>
+</section>
+
+<!--SLIDE 5-->
+<section id="slide-5">
+</section>
+
+<!--SLIDE 6-->
+<section id="slide-6">
+    <section>
+        <h2><span>Порядок работы</span></h2>
+        <div class="workflow">
+            <span>Вы отправляете заявку или заказываете обратный звонок через сайт</span>
+            <span>Вы отправляете заявку или заказываете обратный звонок через сайт</span>
+            <span>Вы отправляете заявку или заказываете обратный звонок через сайт</span>
+            <span>Вы отправляете заявку или заказываете обратный звонок через сайт</span>
+        </div>
+    </section>
+</section>
+
+<!--FOOTER-->
+<footer>
+    <section>
+        <form name="SIMPLE_FORM_7" class="form_block_foot" action="send_vopros.php" method="POST" enctype="multipart/form-data">
+            <input type="text" class="inputtext" name="form_name1" value size="0" placeholder="Имя">
+            <input type="email" class="inputtext" name="form_email1" value size="0" placeholder="E-Mail" required>
+            <input type="text" class="inputtext" name="form_vopros" value size="0" placeholder="Ваш вопрос">
+            <input type="submit" class="ActionButton" id="vp_footer"  name="web_form_submit1"  value="Спросить"  onClick="return Formdata4(this.form)">
+        </form>
+        <div id="question_form_error" style="color: #f00; font-size: 13px;"></div>
+        <div class="contact-info">
+            <div>
+                Группа компаний <span>TANAIS</span>
+                <p>105120 г. Москва ул. Нижняя Сыромятническая д.11</p>
+            </div>
+
+            <div>
+                <a href="tel:+74993909477">
+                    <i></i>
+                    <span>+7 (499) 390-94-77</span>
+                </a>
+
+                <a href="mailto:rasart.pro@gmail.com?subject=Письмо%20с%20LP%20ECM">
+                    <i></i>
+                    <span>Написать письмо</span>
+                </a>
+            </div>
+
+            <div>
+                <a href="#top">Вверх <i></i></a>
+            </div>
+
+            <div></div>
+
+        </div>
+    </section>
+
+</footer>
+
+  <script src="scripts/mian.js"></script>
+
+<?if(isset($_GET["action"]) && ($_GET["action"] == "send_question" || $_GET["action"] == "send_callback")):?>
+<script>
+    $("<div>Спасибо за ваше обращение, наши специалисты обязательно с вами свяжутся, в ближайшее время.</div>").dialog({
+        dialogClass: "info-window",
+        modal: true,
+        draggable: false,
+        buttons: [
+            {
+                text: "OK",
+                click: function() {
+                    $( this ).dialog( "close" );
+                }
+            }
+        ]
+    });
+</script>
+<?elseif(isset($_GET["action"]) && $_GET["action"] == "send_prezent"):?>
+<script>
+    $("<div>Спасибо за ваше обращение,  в течении нескольких минут на ваш Email придет письмо с презентацией.</div>").dialog({
+        dialogClass: "info-window",
+        modal: true,
+        draggable: false,
+        buttons: [
+            {
+                text: "OK",
+                click: function() {
+                    $( this ).dialog( "close" );
+                }
+            }
+        ]
+    });
+</script>
+<?endif;?>
+
+  </body>
+</html>
